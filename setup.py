11c18b475b1e948be6fc355324daea889ed10d60
Andrey Rusanov <rusanov.andrey.mail@gmail.com>
2016-03-08 17:17:41 +0200

    Install stuff

--- setup.py
+++ setup.py
@@ -10,8 +10,22 @@ setup(
     author='Andrey Rusanov',
     author_email='andrey@rusanov.me',
     description='Small lib to manage settings as object for Flask/Bottle/custom apps',
+    long_description=open('README.rst').read(),
     test_requires=[
         'nose==1.3.7',
         'mock==1.3.0'
-    ]
+    ],
+
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 5 - Production/Stable',
+    ],
 )
