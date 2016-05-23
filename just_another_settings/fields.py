497e471defd39e8669cbd8dbf56dae53c3b9b3fc
Andrey Rusanov <rusanov.andrey.mail@gmail.com>
2016-05-23 17:22:04 +0300

    Add file settings

--- just_another_settings/fields.py
+++ just_another_settings/fields.py
@@ -1,23 +1,25 @@
+import json
 import os
 
 
 class BaseField(object):
 
-    def __init__(self, not_none=False):
+    def __init__(self, not_none=True):
         self.not_none = not_none
-        self.value = None
+        self.value = self._fetch_value()
 
     def __get__(self, instance, owner):
-        if self.value:
-            return self.value
-
-        self.value = self.fetch_value()
-        if self.value is None and self.not_none:
-            raise ValueError('Wrong value')
         return self.value
 
     def __set__(self, instance, value):
         self.value = value
+
+    def _fetch_value(self):
+        return self.validate(self.fetch_value())
+
+    def validate(self, value):
+        if value is None and self.not_none:
+            raise ValueError('Wrong value')
 
     def fetch_value(self):
         raise NotImplementedError
@@ -34,3 +36,27 @@ class EnvField(BaseField):
 
     def __set__(self, instance, value):
         raise TypeError('Can not assign value to env field')
+
+
+class BaseFileField(BaseField):
+    def __init__(self, path):
+        self.path = path
+        super(BaseFileField, self).__init__()
+
+    def open(self):
+        with open(self.path) as f:
+            return f.read()
+
+    def parse_file(self, data):
+        raise NotImplementedError
+
+    def fetch_value(self):
+        return self.parse_file(self.open())
+
+
+class JSONFileField(BaseFileField):
+    def open(self):
+        return json.load(self.path)
+
+    def parse_file(self, data):
+        return data
